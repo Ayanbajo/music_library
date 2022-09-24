15c882d9b50a7e60bb01663f71b323c99678cbd6
ayanbajo <bolatitoayanbajo@gmail.com>
2022-09-24 00:43:41 -0400

    Added Suspense

--- src/App.js
+++ src/App.js
@@ -1,49 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
-import { useEffect, useState } from 'react'
+import { useEffect, useState, Suspense } from 'react'
 import Gallery from './components/Gallery'
 import SearchBar from './components/SearchBar'
 import GalleryItem from './components/GalleryItem'
+import {createResource as fetchData} from './helper'
 
 
 const App = () => {
-  let [search, setSearch] = useState('')
+  let [searchTerm, setSearchTerm] = useState('')
   let [message, setMessage] = useState('Search for Music!')
-  let [data, setData] = useState([])
+  let [data, setData] = useState(null)
 
   const API_URL = 'https://itunes.apple.com/search?term='
 
   useEffect(() => {
-      if(search) {
-          const fetchData = async () => {
-              document.title = `Search results for ${search} music`
-              setMessage(`Search results for: ${search}`)
-              const response = await fetch(API_URL + search)
-              const resData = await response.json()
-              // console.log(resData)
-              if (resData.results.length > 0) {
-                  return setData(resData.results)
-              } else {
-                  return setMessage('No Music Found.')
-              }
-          }
-          fetchData()
+      if(searchTerm) {
+            setData(fetchData(searchTerm))
+        //   const fetchData = async () => {
+        //       document.title = `Search results for ${search} music`
+        //       setMessage(`Search results for: ${search}`)
+        //       const response = await fetch(API_URL + search)
+        //       const resData = await response.json()
+        //       // console.log(resData)
+        //       if (resData.results.length > 0) {
+        //           return setData(resData.results)
+        //       } else {
+        //           return setMessage('No Music Found.')
+        //       }
+        //   }
+        //   fetchData()
       }
-  }, [search])
+  }, [searchTerm])
 
   const handleSearch = (e, term) => {
       e.preventDefault()
-      setSearch(term)
+      setSearchTerm(term)
   }
+
+  const renderGallery = () => {
+    if(data){
+        return (
+            <Suspense fallback={<h1>Loading...</h1>}>
+                <Gallery data={data} />
+            </Suspense>
+        )
+    }
+}
 
   return (
       <div>
         <h1 className='App'>{message}</h1>
           <SearchBar handleSearch={handleSearch} />
-          <Gallery data={data} />
+          {renderGallery()}
       </div>
   )
 }
 
-export default App
+export default App;
 
